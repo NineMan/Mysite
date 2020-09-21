c84ce3bc3f8470513f679db31efb889581eca5dd
NineMan <nelubinmv@inbox.ru>
2020-09-21 10:58:40 +0400

    move static dir

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = BASE_DIR + '/' + 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'polls/static'),
+    os.path.join(BASE_DIR, 'polls/../static'),
 )
 
 print('===============================================')
