64da18b4c9bd07ee4a37c075566ce6bc2583a6d1
NineMan <nelubinmv@inbox.ru>
2020-09-29 18:02:42 +0400

    add output on html

--- pyshop/views.py
+++ pyshop/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, live_search
 
+
+# def index(request):
+#     product_list = Product.objects.all()
+#     context = {'product_list': product_list}
+#     return render(request, 'pyshop/livesearch_results.html', context)
 
 def index(request):
     product_list = Product.objects.all()
+    # product_list.live_search()
     context = {'product_list': product_list}
-    return render(request, 'pyshop/index.html', context)
+    return render(request, 'pyshop/livesearch_results.html', context)
